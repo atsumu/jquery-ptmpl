65e2b0f536f22126b774322790693fc8a767c3fc
d4rkie <mousevich@gmail.com>
2012-04-03 21:04:05 +0900

    replaced jQuery notation with $ and added a check for Zepto

--- jquery.ptmpl.js
+++ jquery.ptmpl.js
@@ -7,46 +7,46 @@
  * Dual licensed under the MIT or GPL Version 2 licenses.
  * http://jquery.org/license
  */
-(function (jQuery, undefined) {
+(function ($) {
 
 // jquery extension.
-jQuery.fn.ptmpl = ptmplFn;
-jQuery.ptmpl = ptmplPtmpl;
+$.fn.ptmpl = ptmplFn;
+$.ptmpl = ptmplPtmpl;
 
 // export for plugin.
-jQuery.ptmplDefineTag = ptmplDefineTag;
-jQuery.ptmplUndefineTag = ptmplUndefineTag;
-jQuery.ptmplCompile = ptmplCompile;
-jQuery.ptmplGetCompiled = ptmplGetCompiled;
-jQuery.ptmplEscapeHtml = ptmplEscapeHtml;
-jQuery.ptmplUnescapeHtml = ptmplUnescapeHtml;
-jQuery.ptmplEscapeUrl = encodeURIComponent;
-jQuery.ptmplEscapeStringLiteral = ptmplEscapeStringLiteral;
-jQuery.ptmplTranslateHtmlToLiteral = ptmplEscapeStringLiteral;
-jQuery.ptmplScope = ptmplScope;
-jQuery.ptmplCache = {};
-jQuery.ptmplTagTable = {};
+$.ptmplDefineTag = ptmplDefineTag;
+$.ptmplUndefineTag = ptmplUndefineTag;
+$.ptmplCompile = ptmplCompile;
+$.ptmplGetCompiled = ptmplGetCompiled;
+$.ptmplEscapeHtml = ptmplEscapeHtml;
+$.ptmplUnescapeHtml = ptmplUnescapeHtml;
+$.ptmplEscapeUrl = encodeURIComponent;
+$.ptmplEscapeStringLiteral = ptmplEscapeStringLiteral;
+$.ptmplTranslateHtmlToLiteral = ptmplEscapeStringLiteral;
+$.ptmplScope = ptmplScope;
+$.ptmplCache = {};
+$.ptmplTagTable = {};
 
 function ptmplFn(data, option) {
-	var text = jQuery.ptmplGetCompiled(this[0], option)(option, data);
+	var text = $.ptmplGetCompiled(this[0], option)(option, data);
 	var el = document.createElement('div');
 	el.innerHTML = text;
-	return jQuery(jQuery.makeArray(el.childNodes));
+	return $($.makeArray(el.childNodes));
 }
 
 function ptmplPtmpl(str, data, option) {
-	return jQuery.fn.ptmpl.apply([{ id:null, innerHTML:str }], [data, option]);
+	return $.fn.ptmpl.apply([{ id:null, innerHTML:str }], [data, option]);
 }
 
 function ptmplDefineTag(map) {
-	jQuery.each(map, function (key, proc) {
-		jQuery.ptmplTagTable[key] = proc;
+	$.each(map, function (key, proc) {
+		$.ptmplTagTable[key] = proc;
 	});
 };
 
 function ptmplUndefineTag(list) {
-	jQuery.each(list, function (i, key) {
-		delete jQuery.ptmplTagTable[key];
+	$.each(list, function (i, key) {
+		delete $.ptmplTagTable[key];
 	});
 };
 
@@ -55,24 +55,24 @@ function ptmplCompile(text, option, debugInfo) {
 	var code = [];
 	if (!option.noDebugInfo) code.push('try {');
 	code.push('var _PTMPL_HTML = [];');
-	code.push('with (jQuery.ptmplScope(_PTMPL_ARG)) {');
+	code.push('with ($.ptmplScope(_PTMPL_ARG)) {');
 	text.replace(/((?:a|[^a])*?)(?:\{\{((?:a|[^a])*?)\}\}|$)/g, function (all, html, tag) {
 		if (html) {
-			var line = jQuery.ptmplTranslateHtmlToLiteral(html);
+			var line = $.ptmplTranslateHtmlToLiteral(html);
 			if (line) code.push('_PTMPL_HTML.push("', line, '");');
 		}
 		if (tag) {
 			tag.match(/([^\s\(]+)((?:a|[^a])*)/);
 			var key = RegExp.$1;
 			var str = RegExp.$2;
-			jQuery.ptmplTagTable[key](code, str, option);
+			$.ptmplTagTable[key](code, str, option);
 		}
 	});
 	code.push('}');
 	code.push('return _PTMPL_HTML.join("");');
 	if (!option.noDebugInfo) {
 		code.push('} catch (e) {');
-		code.push('throw new Error("RuntimeError in '+jQuery.ptmplEscapeStringLiteral(debugInfo.id ? debugInfo.id : debugInfo.text.slice(0, 100))+', "+e.message);');
+		code.push('throw new Error("RuntimeError in '+$.ptmplEscapeStringLiteral(debugInfo.id ? debugInfo.id : debugInfo.text.slice(0, 100))+', "+e.message);');
 		code.push('}');
 	}
 	var joined = code.join('');
@@ -94,12 +94,12 @@ function ptmplGetCompiled(elem, option) {
 	var id = elem.id;
 	var text = elem.innerHTML;
 	if (id) {
-		if (id in jQuery.ptmplCache) return jQuery.ptmplCache[id];
-		if (text) return jQuery.ptmplCache[id] = jQuery.ptmplCompile(text, option, { id:"#"+id });
+		if (id in $.ptmplCache) return $.ptmplCache[id];
+		if (text) return $.ptmplCache[id] = $.ptmplCompile(text, option, { id:"#"+id });
 		text = document.getElementById(id).innerHTML;
-		return jQuery.ptmplCache[id] = jQuery.ptmplCompile(text, option, { id:"#"+id });
+		return $.ptmplCache[id] = $.ptmplCompile(text, option, { id:"#"+id });
 	}
-	return jQuery.ptmplCompile(text, option, { text:text });
+	return $.ptmplCompile(text, option, { text:text });
 }
 
 function ptmplEscapeHtml(str) {
@@ -135,18 +135,18 @@ function ptmplScope(map) {
 }
 
 // define tags.
-jQuery.ptmplDefineTag({
+$.ptmplDefineTag({
 	// not escaped output
 	'html': function (code, str) {
 		code.push('_PTMPL_HTML.push((', str, '));');
 	},
 	// html escaped output
 	'=': function (code, str) {
-		code.push('_PTMPL_HTML.push(jQuery.ptmplEscapeHtml((', str, ')));');
+		code.push('_PTMPL_HTML.push($.ptmplEscapeHtml((', str, ')));');
 	},
 	// url escaped output
 	'~': function (code, str) {
-		code.push('_PTMPL_HTML.push(jQuery.ptmplEscapeUrl((', str, ')));');
+		code.push('_PTMPL_HTML.push($.ptmplEscapeUrl((', str, ')));');
 	},
 	// evaluate
 	'$': function (code, str) {
@@ -159,7 +159,7 @@ jQuery.ptmplDefineTag({
 	}});
 
 var el;
-jQuery.ptmplDefineTag({
+$.ptmplDefineTag({
 	'if': function (code, str) {
 		code.push('if (', str, ') {');
 	},
@@ -175,7 +175,7 @@ jQuery.ptmplDefineTag({
 		code.push('}');
 	}});
 
-jQuery.ptmplDefineTag({
+$.ptmplDefineTag({
 	'tryif': function (code, str) {
 		code.push('if ((function () { try { return (', str, '); } catch (e) {} })()) {');
 	},
@@ -190,7 +190,7 @@ jQuery.ptmplDefineTag({
 		code.push('}');
 	}});
 
-jQuery.ptmplDefineTag({
+$.ptmplDefineTag({
 	'for': function (code, str) {
 		code.push('for (', str, ') {');
 	},
@@ -198,14 +198,14 @@ jQuery.ptmplDefineTag({
 		code.push('}');
 	}});
 
-jQuery.ptmplDefineTag({
+$.ptmplDefineTag({
 	'each': function (code, str) {
 		var m = str.match(/\s*\((?:\s*(\w+)\s*(?:,\s*(\w+)\s*))\)\s+((?:a|[^a])+)/);
 		if (m == null) throw 'ptmpl syntax error: near {{each'+str+'}}';
 		var key = RegExp.$1;
 		var val = RegExp.$2;
 		var exp = RegExp.$3;
-		code.push('jQuery.each((', exp, '), function (', key, ',', val, ') {');
+		code.push('$.each((', exp, '), function (', key, ',', val, ') {');
 	},
 	'/each': function (code, str) {
 		code.push('});');
@@ -217,33 +217,33 @@ jQuery.ptmplDefineTag({
 		code.push('return false;');
 	}});
 
-jQuery.ptmplDefineTag({
+$.ptmplDefineTag({
 	'tmpl': function (code, str) {
 		var m = str.match(/\s*\(((?:a|[^a])*?)\)\s+((?:a|[^a])+)/); // TODO: fix imperfect matching
 		if (m == null) throw 'ptmpl syntax error: near {{tmpl'+str+'}}';
 		var arg = RegExp.$1 || '{}';
 		var selector = RegExp.$2;
-		// optimization, avoid jQuery.find() if simple id selector.
+		// optimization, avoid $.find() if simple id selector.
 		var id = selector.match(/^\s*['"]#(.+?)["']\s*$/) && RegExp.$1;
 		if (id) {
-			code.push('_PTMPL_HTML.push(jQuery.ptmplGetCompiled({ id:"', id, '" }, _PTMPL_OPTION)(_PTMPL_OPTION, (', arg, ')));');
+			code.push('_PTMPL_HTML.push($.ptmplGetCompiled({ id:"', id, '" }, _PTMPL_OPTION)(_PTMPL_OPTION, (', arg, ')));');
 		} else {
-			code.push('_PTMPL_HTML.push(jQuery.ptmplGetCompiled(jQuery.find((', selector, '))[0], _PTMPL_OPTION)(_PTMPL_OPTION, (', arg, ')));');
+			code.push('_PTMPL_HTML.push($.ptmplGetCompiled($.find((', selector, '))[0], _PTMPL_OPTION)(_PTMPL_OPTION, (', arg, ')));');
 		}
 	}});
 
-jQuery.ptmplStripPluginTranslateFunctionStack = [];
+$.ptmplStripPluginTranslateFunctionStack = [];
 
-jQuery.ptmplDefineTag({
+$.ptmplDefineTag({
 	'strip': function (code, str) {
-		var old = jQuery.ptmplTranslateHtmlToLiteral;
-		jQuery.ptmplStripPluginTranslateFunctionStack.push(old);
-		jQuery.ptmplTranslateHtmlToLiteral = function (html) {
+		var old = $.ptmplTranslateHtmlToLiteral;
+		$.ptmplStripPluginTranslateFunctionStack.push(old);
+		$.ptmplTranslateHtmlToLiteral = function (html) {
 			return old(html.replace(/\r?\n\t*/g, ''));
 		};
 	},
 	'/strip': function (code, str) {
-		jQuery.ptmplTranslateHtmlToLiteral = jQuery.ptmplStripPluginTranslateFunctionStack.pop();
+		$.ptmplTranslateHtmlToLiteral = $.ptmplStripPluginTranslateFunctionStack.pop();
 	}});
 
-})(jQuery);
+})(window.jQuery || window.Zepto);
